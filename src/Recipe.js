2aadd624e63138a1f9547be5bcfae3bfe18ba601
LunaticHacker <lunatechnology007@gmail.com>
2020-10-18 10:46:02 -0500

    update styles

--- src/Recipe.js
+++ src/Recipe.js
@@ -6,12 +6,14 @@ function Recipe(props) {
     <div className="recipe">
       <h3>{props.title}</h3>
       <img src={props.thumbnail} alt={props.title}></img>
-      <h6>Ingredients</h6>
-      <ol>
-        {props.ingredients.map((ingredient) => (
-          <li key={ingredient + Math.random()}>{ingredient}</li>
-        ))}
-      </ol>
+      <figure>
+        <figcaption>Ingredients</figcaption>
+        <ol>
+          {props.ingredients.map((ingredient) => (
+            <li key={ingredient + Math.random()}>{ingredient}</li>
+          ))}
+        </ol>
+      </figure>
       <a href={props.href}>Try this</a>
     </div>
   );
